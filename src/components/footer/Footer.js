555fe4d2d6aaeefa7563744b0ea634b1cf4862c6
naehyun25 <yoon658@naver.com>
2023-04-16 14:30:50 +0900

    MainProject MouseOver 기능구현완료

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import InnerWrap from '../../UI/InnerWrap';
 
 const Footer =() => {
     return(
-        <div>푸터입니다.</div>
+        <InnerWrap>
+        <div className={styles.FooterIntro}>
+            <div className={styles.contact}>
+                <p>Contact ME,</p>
+                <p>010-5800-2443</p>
+                <p>naehyun25@gamil.com</p>
+                <form>
+                    <button type='e-mail'>메일보내기</button>
+                </form>
+            </div>
+
+
+
+            <div className={styles.remind}>
+                <p>깃허브 바로가기</p>
+                <p>노션 바로가기</p>
+                <p>이력서 바로가기</p>
+            </div>
+        </div>
+        <div>@ 2023. All rights reserved by naehyun25</div>
+        </InnerWrap>
     )
 };
 export default Footer;
--- src/components/main/MainProject.jsx
+++ src/components/main/MainProject.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, {useState,useEffect, useRef} from "react";
 import styles from "./MainProject.module.css";
 import InnerWrap from "../../UI/InnerWrap";
 import project1 from "../../assets/project1.jpg";
@@ -6,78 +6,69 @@ import project2 from "../../assets/project2.jpg";
 import project3 from "../../assets/project3.jpg";
 import { ButtonWhiteSmall } from "../../UI/Buttons";
 import github from "../../assets/github-icon.png";
-import notion from "../../assets/notion-icon.png";
-import resume from "../../assets/resume-icon.png";
 import githubWhite from "../../assets/github-white-icon.png";
-import notionWhite from "../../assets/notion-white-icon.png";
-import resumeWhite from "../../assets/resume-white-icon.png";
 import circlePink from "../../assets/circle-pink.png";
 
-const projectData = [
-    {
-        content : "독립기념관을 리뉴얼했습니다.",
-        date : "3주",
-        contribution : "100%",
-        skill : "HTML, CSS, JS, Jquery",
-        tech : "php게시판, 캐시로그인, 캐로셀",
-        deploy : "GitHub, Dothome",
-        page:"메인1, 게시판1, 로그인1"
-    },
-    {
-        imgUrl : project2,
-        title : "tickatalk",
-        content : "팀프로젝트1. 티켓톡",
-        date : "2주",
-        contribution : "60%",
-        skill : "HTML, CSS, JS, Jquery, bootstrap, Scss",
-        tech : "캐시로그인, 부트스트랩, api",
-        deploy : "GitHub",
-        page:"메인1, 서브페이지1, 로그인1"
-    },
-    {
-        imgUrl : project3,
-        title : "4niture",
-        content : "팀프로젝트2. 가구쇼핑몰",
-        date : "3주",
-        contribution : "70%",
-        skill : "React, CSSmodule, antd, node.js",
-        tech : "리액트, sequlize, 서버구현, sqllite",
-        deploy : "Vercell, CloudType",
-        page:"메인1, 상세상품게시판4, 검색페이지1, 상품업로드1, 리뷰업로드1, 리뷰페이지1"
-    }
+  
+  const ProjectCard = (props) => {
+    const imgRef = useRef(null);
+    const imgCover = useRef();
+    const [translateY, setTranslateY] = useState(0);
+    const [visual, setVisual] = useState(0);
 
-]
-const ProjectCard = (props)=>{
-    let {title,imgUrl,number,skills,date,contribution,page,content,deploy,tech}=props;
-    return(
-        <div className={styles.projectCard}>
-            <div className={styles.projectImgWrap}>
-                <div className={styles.imgCover}>
-                </div>
-                <div className={styles.descWrap}>
-                    <p> {content} </p>
-                    <p>제작기간 {date} </p>
-                    <p>본인기여도 {contribution}</p>
-                    <p>구현페이지 <br/>{page}</p>
-                    <p>Tech <br/>{tech}</p>
-                    <p>배포 {deploy}</p>
-                    <div className={styles.btnWrap}>
-                        <ButtonWhiteSmall name="github" font="fontEnglish" src={github} srcWhite={githubWhite}/>
-                        <ButtonWhiteSmall name="visit" font="fontEnglish" src={github} srcWhite={githubWhite}/>
-                    </div>
-                </div>
-                <img src={imgUrl} alt="프로젝트이미지" className={styles.projectImg}/>
+    const ImgScroll = () => {
+        const imgCoverHeight = imgCover.current.offsetHeight;
+        const scrollHeight = imgRef.current.scrollHeight;
+        const maxTranslateY = scrollHeight - imgCoverHeight;
+        let translateOn = maxTranslateY;
+        setTranslateY(translateOn);
+        setVisual(0.7);
+        
+    };
+    const ImgScrollOff =()=>{
+        let translateOff=0;
+        setTranslateY(translateOff)
+        setVisual(0)
+    }
+    
+    let { title, imgUrl, number, skills, date, contribution, page, content, deploy, tech } = props;
+  
+    return (
+      <div className={styles.projectCard} onMouseOver={ImgScroll} onMouseLeave={ImgScrollOff}>
+        <div className={styles.projectImgWrap}>
+          <div className={styles.imgCover} ref={imgCover} style={{opacity:visual}}></div>
+          <div className={styles.descWrap}>
+            <p> {content} </p>
+            <p>제작기간 {date} </p>
+            <p>본인기여도 {contribution}</p>
+            <p>
+              구현페이지 <br />
+              {page}
+            </p>
+            <p>
+              Tech <br />
+              {tech}
+            </p>
+            <p>배포 {deploy}</p>
+            <div className={styles.btnWrap}>
+              <ButtonWhiteSmall name="github" font="fontEnglish" src={github} srcWhite={githubWhite} />
+              <ButtonWhiteSmall name="visit" font="fontEnglish" src={github} srcWhite={githubWhite} />
             </div>
-            <div className={styles.projectDescWrap}>
-                <p className={styles.projectNumber}>{number}
-                    <img src={circlePink} alt="" className={styles.circlePink} />
-                </p>
-                <div className={`${styles.projectTitle} ${styles[props.font]}`}>{title}</div>
-                <p className={styles.projectSkills}>{skills}</p>
-            </div>
+          </div>
+          <img src={imgUrl} alt="프로젝트이미지" className={styles.projectImg}
+          ref={imgRef}  style={{ transform: `translateY(-${translateY}px)` }}/>
         </div>
+        <div className={styles.projectDescWrap}>
+          <p className={styles.projectNumber}>
+            {number}
+            <img src={circlePink} alt="" className={styles.circlePink} />
+          </p>
+          <div className={`${styles.projectTitle} ${styles[props.font]}`}>{title}</div>
+          <p className={styles.projectSkills}>{skills}</p>
+        </div>
+      </div>
     );
-}
+  };
 const MainProject = () => {
     return(
         <>
